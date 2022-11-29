9c4467f8684afb7beca52994c647df4312a96ae1
Talitahtml <97572310+Talitahtml@users.noreply.github.com>
2022-11-29 18:16:33 -0300

    Add files via upload

---
+++ Projetocap3.java
@@ -0,0 +1,24 @@
+
+package projetocap3;
+
+/**
+ *
+ * @author elica
+ */
+public class Projetocap3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 1;
+        int y = 5;
+        
+        int soma = x + y;
+        
+        System.out.println("o resultado de x + y é =" + soma);
+    }
+    
+}
+ 
